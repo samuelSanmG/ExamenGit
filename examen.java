bbda9afa2e7b96f6a3349dd247c5c416a5f43854
Samuel San Miguel <samuelsanmiguel13@gmail.com>
2026-02-06 10:08:36 +0100

    Version final

--- examen.java
+++ examen.java
@@ -1,6 +1,9 @@
 public static void multiplos(int numerosMax){
 
     for (int i=0;i<=numerosMax;i++){
+        if(i%7==0){
+            System.out.println("Múltiplo de 7:" +i);
+        }
         if(i%49==0){
             System.out.println("Múltiplo de 49:" +i);
         }
